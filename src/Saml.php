fc9a908b463d8e3c7298adfae34420b06798fefe
Sergey Akudovich <asminog@asminog.com>
2018-04-24 17:31:58 +0300

    config from array

--- src/Saml.php
+++ src/Saml.php
@@ -26,15 +26,18 @@ class Saml extends Object
      * Configurations for OneLogin_Saml2_Auth.
      * @var array
      */
-    private $config;
+    public $config;
 
     public function init()
     {
         parent::init();
 
-        $configFile = Yii::getAlias($this->configFileName);
+        if (!isset($this->config)) {
+            $configFile = Yii::getAlias($this->configFileName);
 
-        $this->config = require($configFile);
+            $this->config = require($configFile);
+        }
+
         $this->instance = new \OneLogin_Saml2_Auth($this->config);
     }
 
