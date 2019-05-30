6e5b445a4061683cda3f1b32c16021fceae9095a
Sergey Akudovich <asminog@asminog.com>
2019-05-30 16:25:13 +0300

    remove test

--- tests/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-error_reporting(-1);
-define('YII_ENABLE_ERROR_HANDLER', false);
-define('YII_DEBUG', true);
-
-require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
-
-Yii::setAlias("@app", __DIR__);
